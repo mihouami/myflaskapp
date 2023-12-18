5393f072b2fb294895b943fd0e9d38a7ef3d57a0
Minou <am.mihoubi@gmail.com>
2023-12-18 19:29:16 +0100

    add api exchange model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from csv import DictWriter
 from io import BytesIO, StringIO
-from requests import get
+from requests import get, ConnectionError
 from json import dumps
 
 
@@ -21,6 +21,7 @@ class Upload(db.Model):
     added = db.Column(db.DateTime, default=datetime.utcnow())
     deleted = db.Column(db.Boolean, default=False)
 
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -28,22 +29,60 @@ class Item(db.Model):
     deleted = db.Column(db.Boolean, default=False)
 
 
-@app.route('/api')
+@app.route("/apiex", methods=["POST", "GET"])
+def apiex():
+    data = {"ABC": 0.5, "DEF": 2.5, "GHI": 5}
+
+    if request.method == "POST":
+        in_value = request.form["amount"]
+        from_rate = request.form["from_rate"]
+        from_to_eur = int(in_value) / float(from_rate)
+        to_rate = request.form["to_rate"]
+        from_eur_to = from_to_eur * float(to_rate)
+        return render_template(
+            "apiex.html",
+            data=data,
+            in_value=in_value,
+            from_rate=from_rate,
+            to_rate=to_rate,
+            from_eur_to=from_eur_to,
+            from_to_eur=from_to_eur,
+        )
+    else:
+        return render_template("apiex.html", data=data)
+
+
+"""
+@app.route('/apiex')
+def apiex():
+    key = "d2e51f7fdee4e9d0f617bb70c6369f35"
+    url = "http://api.exchangeratesapi.io/v1/latest?access_key=d2e51f7fdee4e9d0f617bb70c6369f35"
+    r = get(url)
+    data = r.json()
+    amount = request.form['amount']
+    rate = request.args.get('rate')
+    out_amount = amount * rate
+
+    return render_template("apiex.html", data=data["rates"], out_amount=out_amount, amount=amount)
+"""
+
+
+@app.route("/api", methods=["POST", "GET"])
 def api():
-    api_key = 'xqCVL7TbJCBIrEan8D97cAtuAxg12tka6ZWLawnx'
+    api_key = "xqCVL7TbJCBIrEan8D97cAtuAxg12tka6ZWLawnx"
     endpoint = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
     r = get(endpoint)
     data = r.json()
 
-    return render_template('api.html', data=data)
+    return render_template("api.html", data=data)
 
-@app.route('/pagination')
+
+@app.route("/pagination")
 def show_items():
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
     pages = int(request.args.get("pages", "5"))
     pagination = Item.query.paginate(page=page, per_page=pages, error_out=False)
-    return render_template('pagination.html', pagination=pagination, pages=pages)
-
+    return render_template("pagination.html", pagination=pagination, pages=pages)
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -64,18 +103,24 @@ def home():
 
 @app.route("/download", methods=["POST"])
 def download():
-
     items = Item.query.filter(Item.deleted == False).all()
 
     # Create CSV data in memory
     csv_data = BytesIO()
-    #csv_writer = DictWriter(csv_data, fieldnames=['ID', 'Name', 'Added', 'Deleted'])
+    # csv_writer = DictWriter(csv_data, fieldnames=['ID', 'Name', 'Added', 'Deleted'])
 
     # Encode CSV data before writing
-    csv_data.write(','.join(['ID', 'Name', 'Added', 'Deleted']).encode('utf-8') + b'\n')  # Writing headers
+    csv_data.write(
+        ",".join(["ID", "Name", "Added", "Deleted"]).encode("utf-8") + b"\n"
+    )  # Writing headers
 
     for item in items:
-        row = ','.join([str(item.id), item.name, str(item.added), str(item.deleted)]).encode('utf-8') + b'\n'
+        row = (
+            ",".join(
+                [str(item.id), item.name, str(item.added), str(item.deleted)]
+            ).encode("utf-8")
+            + b"\n"
+        )
         csv_data.write(row)
 
     csv_data.seek(0)  # Reset the file pointer to the beginning of the file-like object
@@ -86,7 +131,7 @@ def download():
             csv_data,
             as_attachment=True,
             download_name="Download.csv",
-            mimetype='text/csv'
+            mimetype="text/csv",
         )
     except Exception as e:
         return jsonify({"error": f"Failed to send file for download: {str(e)}"}), 500
@@ -100,6 +145,7 @@ def search():
     ).all()
     return render_template("search.html", items=items, search_query=search_query)
 
+
 @app.route("/download/search", methods=["POST"])
 def download_search():
     search_query = request.form.get("search_query")
@@ -110,13 +156,20 @@ def download_search():
 
     # Create CSV data in memory
     csv_data = BytesIO()
-    #csv_writer = DictWriter(csv_data, fieldnames=['ID', 'Name', 'Added', 'Deleted'])
+    # csv_writer = DictWriter(csv_data, fieldnames=['ID', 'Name', 'Added', 'Deleted'])
 
     # Encode CSV data before writing
-    csv_data.write(','.join(['ID', 'Name', 'Added', 'Deleted']).encode('utf-8') + b'\n')  # Writing headers
+    csv_data.write(
+        ",".join(["ID", "Name", "Added", "Deleted"]).encode("utf-8") + b"\n"
+    )  # Writing headers
 
     for item in items:
-        row = ','.join([str(item.id), item.name, str(item.added), str(item.deleted)]).encode('utf-8') + b'\n'
+        row = (
+            ",".join(
+                [str(item.id), item.name, str(item.added), str(item.deleted)]
+            ).encode("utf-8")
+            + b"\n"
+        )
         csv_data.write(row)
 
     csv_data.seek(0)  # Reset the file pointer to the beginning of the file-like object
@@ -127,10 +180,11 @@ def download_search():
             csv_data,
             as_attachment=True,
             download_name=f"search_results_{search_query}.csv",
-            mimetype='text/csv'
+            mimetype="text/csv",
         )
     except Exception as e:
         return jsonify({"error": f"Failed to send file for download: {str(e)}"}), 500
+
 
 """
 @app.route("/download/search", methods=["POST"])
@@ -152,6 +206,7 @@ def download_search():
     # Send the created CSV file for download
     return send_file(csv_filename, as_attachment=True)
 """
+
 
 @app.route("/update/<int:id>", methods=["POST", "GET"])
 def update(id):
@@ -176,7 +231,7 @@ def delete(id):
         return redirect("/")
     except:
         return "Something went wrong"
-    
+
 
 @app.route("/delete/def/<int:id>")
 def delete_def(id):
@@ -187,7 +242,7 @@ def delete_def(id):
         return redirect("/deleted")
     except:
         return "Something went wrong"
-    
+
 
 @app.route("/restore/<int:id>")
 def restore(id):
@@ -198,22 +253,20 @@ def restore(id):
         return redirect("/deleted")
     except:
         return "Something went wrong"
-    
+
 
 @app.route("/upload", methods=["POST", "GET"])
 def upload():
     files = Upload.query.order_by(Item.added.desc()).filter(Item.deleted == False).all()
-    if request.method == 'POST':
-        file = request.files['file']
+    if request.method == "POST":
+        file = request.files["file"]
         upload = Upload(filename=file.filename, data=file.read())
         db.session.add(upload)
         db.session.commit()
-        return redirect('/upload')
-    return render_template('upload.html', files=files)
+        return redirect("/upload")
+    return render_template("upload.html", files=files)
 
 
-
-    
 @app.route("/delete/def/file/<int:id>")
 def delete_def_file(id):
     file = Upload.query.get_or_404(id)
@@ -223,7 +276,7 @@ def delete_def_file(id):
         return redirect("/deleted")
     except:
         return "Something went wrong"
-    
+
 
 @app.route("/delete/file/<int:id>")
 def delete_file(id):
@@ -235,7 +288,7 @@ def delete_file(id):
     except:
         return "Something went wrong"
 
-    
+
 @app.route("/restore/file/<int:id>")
 def restore_file(id):
     file = Upload.query.get_or_404(id)
@@ -247,13 +300,12 @@ def restore_file(id):
         return "Something went wrong"
 
 
-@app.route('/download/<upload_id>')
+@app.route("/download/<upload_id>")
 def download_file(upload_id):
     download = Upload.query.filter_by(id=upload_id).first()
-    return send_file(BytesIO(download.data), download_name=download.filename, as_attachment=True)
-
-
-
+    return send_file(
+        BytesIO(download.data), download_name=download.filename, as_attachment=True
+    )
 
 
 @app.route("/deleted")
@@ -269,18 +321,36 @@ def sort():
     sort2 = request.args.get("sort2", "asc")
     if sort2 == "asc":
         if sort1 == "name":
-            items = Item.query.order_by(Item.name.asc()).filter(Item.deleted == False).all()
+            items = (
+                Item.query.order_by(Item.name.asc()).filter(Item.deleted == False).all()
+            )
         elif sort1 == "id":
-            items = Item.query.order_by(Item.id.asc()).filter(Item.deleted == False).all()
+            items = (
+                Item.query.order_by(Item.id.asc()).filter(Item.deleted == False).all()
+            )
         else:
-            items = Item.query.order_by(Item.added.asc()).filter(Item.deleted == False).all()
+            items = (
+                Item.query.order_by(Item.added.asc())
+                .filter(Item.deleted == False)
+                .all()
+            )
     else:
         if sort1 == "name":
-            items = Item.query.order_by(Item.name.desc()).filter(Item.deleted == False).all()
+            items = (
+                Item.query.order_by(Item.name.desc())
+                .filter(Item.deleted == False)
+                .all()
+            )
         elif sort1 == "id":
-            items = Item.query.order_by(Item.id.desc()).filter(Item.deleted == False).all()
+            items = (
+                Item.query.order_by(Item.id.desc()).filter(Item.deleted == False).all()
+            )
         else:
-            items = Item.query.order_by(Item.added.desc()).filter(Item.deleted == False).all()
+            items = (
+                Item.query.order_by(Item.added.desc())
+                .filter(Item.deleted == False)
+                .all()
+            )
 
     return render_template("sort.html", items=items, sort1=sort1, sort2=sort2)
 
